96ededf6f6dfe10e2319d1cab22625c2dc11d732
cromagnon <crocmagnus@gmail.com>
2017-07-22 08:48:34 -0700

    style: Fixed indentation, added comments

--- src/App.js
+++ src/App.js
@@ -7,27 +7,37 @@ import escapeRegExp from 'escape-string-regexp'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: [],
     query: ''
   }
 
+  /**
+  * @description Updates query with the user's input. Called each time user
+  *              enters text into input box.
+  * @param {string} query
+  */
   updateQuery = (query) => {
     this.setState({ query: query.trim() })
   }
 
+  /**
+  * @description Updates books collection with the one fetched from the
+  *              backend. Called when component has been mounted.
+  */
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({books})
     });
   }
 
+  /**
+  * @description Updates the books collection with a re-shelved book.
+  *              Takes book to be re-shelved, and shelf to be re-shelved to.
+  *              Calls backend to update books collection.
+  * @param {string} book
+  * @param {string} shelf
+  * @returns {number} Sum of a and b
+  */
   changeBookShelf = (book, shelf) => {
     this.setState((state) => ({
       books: state.books.map((b) => {
@@ -41,6 +51,9 @@ class BooksApp extends React.Component {
     BooksAPI.update(book, shelf);
   }
 
+  /**
+  * @description Clears the state query variable
+  */
   clearQuery = () => {
     this.setState({query: ''})
   }
@@ -60,27 +73,27 @@ class BooksApp extends React.Component {
 
     return (
       <div className="app">
-      <Route exact path="/" render={({location}) => (
-          <div className="list-books">
-            <div className="list-books-results">
-              <ListBooks books={bookResults} onChangeBookShelf={this.changeBookShelf} location={location} />
-            </div>
-          </div>
-        )}/>
-
-      <Route path="/search" render={({location}) => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link to="/" className="close-search" onClick={this.clearQuery}>Close</Link>
-              <div className="search-books-input-wrapper">
-                <input type="text" value={query} onChange={(e) => (this.updateQuery(e.target.value))} placeholder="Search by title or author"/>
+        <Route exact path="/" render={({location}) => (
+            <div className="list-books">
+              <div className="list-books-results">
+                <ListBooks books={bookResults} onChangeBookShelf={this.changeBookShelf} location={location} />
               </div>
             </div>
-            <div className="search-books-results">
-                <ListBooks books={bookResults} onChangeBookShelf={this.changeBookShelf} location={location} />
+          )}/>
+
+        <Route path="/search" render={({location}) => (
+            <div className="search-books">
+              <div className="search-books-bar">
+                <Link to="/" className="close-search" onClick={this.clearQuery}>Close</Link>
+                <div className="search-books-input-wrapper">
+                  <input type="text" value={query} onChange={(e) => (this.updateQuery(e.target.value))} placeholder="Search by title or author"/>
+                </div>
+              </div>
+              <div className="search-books-results">
+                  <ListBooks books={bookResults} onChangeBookShelf={this.changeBookShelf} location={location} />
+              </div>
             </div>
-          </div>
-      )} />
+        )} />
       </div>
     )
   }
---
+++ src/BookShelfData.js
@@ -0,0 +1,24 @@
+const BOOK_SHELF_DATA = [
+	{
+		key: 'moveTo',
+		text: 'Move to...'
+	},
+	{
+		key: 'currentlyReading',
+		text: 'Currently Reading'
+	},
+	{
+		key: 'wantToRead',
+		text: 'Want to Read'
+	},
+	{
+		key: 'read',
+		text: 'Read'
+	},
+	{
+		key: 'none',
+		text: 'None'
+	}
+]
+
+export default BOOK_SHELF_DATA
